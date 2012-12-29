9da234779ad954d6ffd63ffbdb519b874b66cb47
Javi Merino <cibervicho@gmail.com>
2012-12-29 19:56:39 +0100

    Add a validate script

---
+++ validate.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use WebService::Validator::HTML::W3C;
+use common::sense;
+
+my $file = "wwii.html";
+my $v = WebService::Validator::HTML::W3C->new(
+    detailed    =>  1
+    );
+
+if ($v->validate_file($file)) {
+    if ($v->is_valid) {
+        printf("$file is valid\n");
+        exit(0);
+    } else {
+        printf("$file is not valid\n");
+        foreach my $error (@{$v->errors}) {
+            printf("%s at line %d\n", $error->msg,
+                   $error->line);
+        }
+        exit(-1);
+    }
+} else {
+    printf("Failed to validate the file: %s\n", $v->validator_error);
+    exit(-1);
+}
