20757aeb43e9c35ab26413df22847bc6af207a14
Javi Merino <cibervicho@gmail.com>
2012-12-30 21:00:13 +0100

    validate: don't exit from the function

--- validate.pl
+++ validate.pl
@@ -13,19 +13,23 @@ sub validate_html()
     if ($v->validate_file($file)) {
         if ($v->is_valid) {
             printf("$file is valid\n");
-            exit(0);
+            return 0;
         } else {
             printf("$file is not valid\n");
             foreach my $error (@{$v->errors}) {
                 printf("%s at line %d\n", $error->msg,
                        $error->line);
             }
-            exit(-1);
+            return -1;
         }
     } else {
         printf("Failed to validate the file: %s\n", $v->validator_error);
-        exit(-1);
+        return -1;
     }
 }
 
-validate_html();
+my $ret = 0;
+
+$ret |= validate_html();
+
+exit($ret);
