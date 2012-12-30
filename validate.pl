139d4bffddd4a8a403688f5d9e89ea88fcb7b2aa
Javi Merino <cibervicho@gmail.com>
2012-12-30 20:48:01 +0100

    validate: refactor html validation in a function

--- validate.pl
+++ validate.pl
@@ -3,24 +3,29 @@
 use WebService::Validator::HTML::W3C;
 use common::sense;
 
-my $file = "wwii.html";
-my $v = WebService::Validator::HTML::W3C->new(
-    detailed    =>  1
-    );
+sub validate_html()
+{
+    my $file = "wwii.html";
+    my $v = WebService::Validator::HTML::W3C->new(
+        detailed    =>  1
+        );
 
-if ($v->validate_file($file)) {
-    if ($v->is_valid) {
-        printf("$file is valid\n");
-        exit(0);
-    } else {
-        printf("$file is not valid\n");
-        foreach my $error (@{$v->errors}) {
-            printf("%s at line %d\n", $error->msg,
-                   $error->line);
+    if ($v->validate_file($file)) {
+        if ($v->is_valid) {
+            printf("$file is valid\n");
+            exit(0);
+        } else {
+            printf("$file is not valid\n");
+            foreach my $error (@{$v->errors}) {
+                printf("%s at line %d\n", $error->msg,
+                       $error->line);
+            }
+            exit(-1);
         }
+    } else {
+        printf("Failed to validate the file: %s\n", $v->validator_error);
         exit(-1);
     }
-} else {
-    printf("Failed to validate the file: %s\n", $v->validator_error);
-    exit(-1);
 }
+
+validate_html();
