d30a9babfb71ff22f983dc0ca3a296663048a3b9
Javi Merino <cibervicho@gmail.com>
2012-12-31 18:28:02 +0100

    show people relevant only to the current year

--- base.js
+++ base.js
@@ -1,4 +1,22 @@
+function show_person(n, name, show) {
+    if (n.innerText.indexOf(name) != -1) {
+        if (show)
+            n.style.display = "";
+        else
+            n.style.display = "none";
+    }
+}
+
 function updateTimer(year)
 {
     document.getElementById("selected_year").innerHTML = year;
+
+    var people = document.getElementById("people").getElementsByClassName("person");
+    var i;
+    for (i = 0; i < people.length; i++) {
+        var childNode = people[i];
+        show_person(childNode, "Churchill", year >= 1940);
+        show_person(childNode, "Stalin", year >= 1941);
+        show_person(childNode, "Roosevelt", year >= 1941);
+    }
 }
