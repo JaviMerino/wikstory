166babe500db5e296f016817d2d877f9297e688b
Javi Merino <cibervicho@gmail.com>
2013-02-23 09:47:20 +0000

    add pushpins to the map

--- base.js
+++ base.js
@@ -3,6 +3,40 @@ var topic = {
         center: {lat: 51.869, lon: 14.64},
         zoom: 4,
     },
+    events: {
+        1939: [
+            { lat: 51.869, lon: 14.64, text: "Germany invades Poland" },
+        ],
+        1940: [
+            {lat: 49.74, lon: 4.81, text: "Germany invades France"},
+            {lat: 51.86, lon: -0.87, text: "Battle of Britain"},
+        ],
+        1941: [
+            {lat: 55.61, lon: 26.5, text: "Germany attacks the Soviet Union"},
+            {lat: 21.33, lon: -157.98, text: "Japanese attack on Pearl Harbor"},
+        ],
+        1942: [
+            {lat: 40.3, lon: -73.4, text: "Second Happy Time"},
+            {lat: 28.2, lon: -177.35, text: "Battle of Midway"},
+            {lat: 48.67, lon: 44.3, text: "Battle of Stalingrad"},
+            {lat: 30.837, lon: 28.98, text: "Battles of El Alamein"},
+        ],
+        1943: [
+            {lat: 7.12, lon: 171.1, text: "Gilbert and Marshall Islands Campaign"},
+            {lat: 37.65, lon: 14.05, text: "Allied invasion of Italy"},
+            {lat: 52.97, lon: 36.07, text: "Operation Kutuzov"},
+        ],
+        1944: [
+            {lat: 49.37, lon: 0.88, text: "Normandy Landings"},
+            {lat: 53.89, lon: 28.24, text: "Operation Bagration"},
+            {lat: 20, lon: 130, text: "Battle of the Philippines"},
+        ],
+        1945: [
+            {lat: 52.52, lon: 13.37, text: "Germany surrenders"},
+            {lat: 24.78, lon: 141.32, text: "Battle of Iwo Jima"},
+            {lat: 36, lon: 139.6, text: "Japan surrenders"},
+        ],
+    },
 };
 
 function showPerson(n, name, show) {
@@ -26,6 +60,9 @@ function updateTimer(year)
         showPerson(childNode, "Stalin", year >= 1941);
         showPerson(childNode, "Roosevelt", year >= 1941);
     }
+
+    if (map)
+        map.update(year);
 }
 document.addEventListener("DOMContentLoaded", function(){updateTimer(document.getElementById("selected_year").innerHTML)});
 
@@ -122,5 +159,34 @@ function initializeMap()
     };
 
     map = new Microsoft.Maps.Map(document.getElementById("map_container"), map_options);
+
+    map.shown_pushpins = [];
+
+    map.addPushpin = function(m) {
+        var latlon = new Microsoft.Maps.Location(m.lat, m.lon);
+        var pushpin = new Microsoft.Maps.Pushpin(latlon, {text: m.text});
+
+        map.entities.push(pushpin);
+        this.shown_pushpins.push(pushpin);
+    }
+
+    map.removePushpins = function() {
+        for (var i = 0, p; p = this.shown_pushpins[i]; i++)
+            map.entities.remove(p);
+        this.shown_pushpins.length = 0;
+    }
+
+    map.updatePushpins = function(year) {
+        this.removePushpins();
+
+        for (var i = 0, p; p = topic.events[year][i]; i++)
+            this.addPushpin(p);
+    }
+
+    map.update = function(year) {
+        map.updatePushpins(year);
+    }
+
+    map.update(document.getElementById("selected_year").innerHTML);
 }
 window.addEventListener("load", initializeMap);
